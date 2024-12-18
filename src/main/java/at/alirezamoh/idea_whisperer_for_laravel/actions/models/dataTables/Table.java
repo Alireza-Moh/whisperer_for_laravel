d25e6d52d53d0922cb2e3dea43afcd7f61a917be
Alireza Mohammadi <mohammadialireza238@gmail.com>
2024-12-18 11:44:36 +0100

    Searches for migration files globally & adds table file resolving.

--- src/main/java/at/alirezamoh/idea_whisperer_for_laravel/actions/models/dataTables/Table.java
+++ src/main/java/at/alirezamoh/idea_whisperer_for_laravel/actions/models/dataTables/Table.java
@@ -1,6 +1,8 @@
 package at.alirezamoh.idea_whisperer_for_laravel.actions.models.dataTables;
 
+import com.jetbrains.php.lang.psi.elements.impl.MethodReferenceImpl;
+
 import java.util.List;
 
-public record Table(String name, List<Field> fields) {
+public record Table(String name, MethodReferenceImpl navigationElement, List<Field> fields) {
 }
