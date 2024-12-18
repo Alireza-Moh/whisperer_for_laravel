86721761ab605bec4cd436028090225a3c21d809
Alireza Mohammadi <mohammadialireza238@gmail.com>
2024-12-18 12:38:34 +0100

    Adds more method for inertia page autocompletion.

--- src/main/java/at/alirezamoh/idea_whisperer_for_laravel/inertia/InertiaReferenceContributor.java
+++ src/main/java/at/alirezamoh/idea_whisperer_for_laravel/inertia/InertiaReferenceContributor.java
@@ -1,6 +1,5 @@
 package at.alirezamoh.idea_whisperer_for_laravel.inertia;
 
-import at.alirezamoh.idea_whisperer_for_laravel.blade.BladeReference;
 import at.alirezamoh.idea_whisperer_for_laravel.support.laravelUtils.ClassUtils;
 import at.alirezamoh.idea_whisperer_for_laravel.support.laravelUtils.FrameworkUtils;
 import at.alirezamoh.idea_whisperer_for_laravel.support.laravelUtils.MethodUtils;
@@ -23,14 +22,26 @@ public class InertiaReferenceContributor extends PsiReferenceContributor {
     /**
      * The names of the methods in the 'View' facade that can reference Blade files
      */
-    public static Map<String, List<Integer>> Inertia_METHODS = new HashMap<>() {{
-        put("render", List.of(0));
+    public static Map<String, Integer> INERTIA_METHODS = new HashMap<>() {{
+        put("render", 0);
+    }};
+
+    /**
+     * The names of the methods in the 'View' facade that can reference Blade files
+     */
+    public static Map<String, Integer> ROUTE_INERTIA_METHODS = new HashMap<>() {{
+        put("inertia", 0);
     }};
 
     /**
      * The FQN of the 'Route' facade
      */
     private final String INERTIA = "\\Inertia\\Inertia";
+
+    /**
+     * The FQN of the 'Route' facade
+     */
+    private final String ROUTE = "\\Illuminate\\Support\\Facades\\Route";
 
     @Override
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar psiReferenceRegistrar) {
@@ -44,7 +55,7 @@ public class InertiaReferenceContributor extends PsiReferenceContributor {
                         return PsiReference.EMPTY_ARRAY;
                     }
 
-                    if (isInsideViewMethods(psiElement)) {
+                    if (isInsideCorrectMethod(psiElement)) {
                         String text = psiElement.getText();
 
                         return new PsiReference[]{new InertiaReference(
@@ -60,48 +71,98 @@ public class InertiaReferenceContributor extends PsiReferenceContributor {
     }
 
     /**
-     * Checks if the given PSI element is inside inertia method
-     * @param psiElement The PSI element to check
-     * @return True or false
+     * Checks if a given PSI element is inside a valid Inertia method, Route method, or function.
+     *
+     * @param psiElement The PSI element to check.
+     * @return True if the element is inside a valid method or function, false otherwise.
      */
-    private boolean isInsideViewMethods(@NotNull PsiElement psiElement) {
+    private boolean isInsideCorrectMethod(@NotNull PsiElement psiElement) {
         MethodReference method = MethodUtils.resolveMethodReference(psiElement, 10);
         Project project = psiElement.getProject();
 
-        return method != null
-            && isPageParam(method, psiElement)
-            && isInertiaMethod(method, project);
+        if (method != null) {
+            return isInertiaMethod(method, psiElement, project) || isRouteMethod(method, psiElement, project);
+        }
+
+        return isInertiaFunction(psiElement);
     }
 
     /**
-     * General method to check if the given reference and position match the view parameter criteria
-     * @param reference The method or function reference
-     * @param position The PSI element position
-     * @return True or false
+     * Checks if a given method reference matches an Inertia method.
+     *
+     * @param methodReference The method reference.
+     * @param position        The PSI element position.
+     * @param project         The current project.
+     * @return                true or false
      */
-    private boolean isPageParam(PsiElement reference, PsiElement position) {
-        int paramIndex = MethodUtils.findParamIndex(position, false);
-        String referenceName = (reference instanceof MethodReference)
-            ? ((MethodReference) reference).getName()
-            : ((FunctionReference) reference).getName();
+    private boolean isInertiaMethod(MethodReference methodReference, PsiElement position, Project project) {
+        return isExpectedMethod(methodReference, position, project, INERTIA_METHODS, INERTIA);
+    }
 
-        List<Integer> paramPositions = Inertia_METHODS.get(referenceName);
+    /**
+     * Checks if a given method reference matches a Route method.
+     *
+     * @param methodReference The method reference.
+     * @param position        The PSI element position.
+     * @param project         The current project.
+     * @return                true or false
+     */
+    private boolean isRouteMethod(MethodReference methodReference, PsiElement position, Project project) {
+        return isExpectedMethod(methodReference, position, project, ROUTE_INERTIA_METHODS, ROUTE);
+    }
 
-        return paramPositions != null && paramPositions.contains(paramIndex);
+    /**
+     * Generalized logic for matching method references to a specific class and parameter criteria.
+     *
+     * @param methodReference   The method reference.
+     * @param position          The PSI element position.
+     * @param project           The current project.
+     * @param methodMap         Map of method names and their parameter indices.
+     * @param expectedClassFQN  Fully qualified name of the expected class.
+     * @return                  true or false
+     */
+    private boolean isExpectedMethod(
+        MethodReference methodReference,
+        PsiElement position,
+        Project project,
+        Map<String, Integer> methodMap,
+        String expectedClassFQN
+    ) {
+        Integer expectedParamIndex = methodMap.get(methodReference.getName());
+
+        if (expectedParamIndex == null) {
+            return false;
+        }
+
+        List<PhpClassImpl> resolvedClasses = getPhpClassesForMethod(methodReference, project);
+        PhpClass expectedClass = ClassUtils.getClassByFQN(project, expectedClassFQN);
+
+        return expectedClass != null
+            && resolvedClasses.stream().anyMatch(clazz -> ClassUtils.isChildOf(clazz, expectedClass))
+            && expectedParamIndex == MethodUtils.findParamIndex(position, false);
+    }
+
+    private @NotNull List<PhpClassImpl> getPhpClassesForMethod(MethodReference methodReference, Project project) {
+        return MethodUtils.resolveMethodClasses(methodReference, project);
     }
 
     /**
      * Checks if the given method reference is a view or route method
-     * @param methodReference The method reference
-     * @param project The project context
-     * @return True or false
+     * @param position psi element
+     * @return         true or false
      */
-    private boolean isInertiaMethod(MethodReference methodReference, Project project) {
-        List<PhpClassImpl> resolvedClasses = MethodUtils.resolveMethodClasses(methodReference, project);
+    private boolean isInertiaFunction(PsiElement position) {
+        FunctionReference functionReference = MethodUtils.resolveFunctionReference(position, 10);
+        if (functionReference == null) {
+            return false;
+        }
 
-        PhpClass inertiaClass = ClassUtils.getClassByFQN(project, INERTIA);
+        Integer expectedParamIndex = ROUTE_INERTIA_METHODS.get(functionReference.getName());
 
-        return inertiaClass != null
-            && resolvedClasses.stream().anyMatch(clazz -> ClassUtils.isChildOf(clazz, inertiaClass));
+        if (expectedParamIndex == null) {
+            return false;
+        }
+
+        return expectedParamIndex == MethodUtils.findParamIndex(position, false);
     }
 }
