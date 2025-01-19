de38600ece3b4968cf5ec93f5d255ac5a06ff5c5
Alireza Mohammadi <mohammadialireza238@gmail.com>
2025-01-19 19:42:07 +0100

    Updates model action id.

--- src/main/java/at/alirezamoh/whisperer_for_laravel/actions/EloquentNonGroupAction.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/actions/EloquentNonGroupAction.java
@@ -3,15 +3,23 @@ package at.alirezamoh.whisperer_for_laravel.actions;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import org.jetbrains.annotations.NotNull;
 
 public class EloquentNonGroupAction extends BaseAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        AnAction secondAction = ActionManager.getInstance().getAction("create.EloquentAction");
+        ActionManager actionManager = ActionManager.getInstance();
+        AnAction modelAction = actionManager.getAction("at.alirezamoh.whisperer_for_laravel.EloquentAction");
 
-        if (secondAction != null) {
-            secondAction.actionPerformed(anActionEvent);
+        if (modelAction != null) {
+            actionManager.tryToExecute(
+                modelAction,
+                anActionEvent.getInputEvent(),
+                anActionEvent.getDataContext().getData(PlatformDataKeys.CONTEXT_COMPONENT),
+                anActionEvent.getPlace(),
+                true
+            );
         }
     }
 }
