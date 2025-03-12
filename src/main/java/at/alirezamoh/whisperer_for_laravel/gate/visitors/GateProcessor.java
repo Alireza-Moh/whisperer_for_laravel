b3010e4a44dd79b699c180ed2b612d3ed8d4a40e
Alireza Mohammadi <mohammadialireza238@gmail.com>
2025-03-12 19:05:28 +0100

    [feature]: adds support for policy completion.

--- src/main/java/at/alirezamoh/whisperer_for_laravel/gate/visitors/GateProcessor.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/gate/visitors/GateProcessor.java
@@ -1,11 +1,16 @@
 package at.alirezamoh.whisperer_for_laravel.gate.visitors;
 
+import at.alirezamoh.whisperer_for_laravel.indexes.PolicyIndex;
 import at.alirezamoh.whisperer_for_laravel.support.applicationModules.visitors.BaseServiceProviderVisitor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.util.indexing.IdFilter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GateProcessor {
@@ -19,7 +24,11 @@ public class GateProcessor {
         GateModuleServiceProviderVisitor visitor = new GateModuleServiceProviderVisitor(project);
         traverseAndAccept(visitor);
 
-        return visitor.getVariants();
+        List<String> variants = visitor.getVariants();
+
+        variants.addAll(collectPolicies(project));
+
+        return variants;
     }
 
     public PsiElement findGateAbility(PsiElement element) {
@@ -33,5 +42,30 @@ public class GateProcessor {
         for (PsiFile serviceProvider : BaseServiceProviderVisitor.getProviders(project)) {
             serviceProvider.acceptChildren(visitor);
         }
+    }
+
+    private List<String> collectPolicies(Project project) {
+        List<String> policies = new ArrayList<>();
+        FileBasedIndex fileBasedIndex = FileBasedIndex.getInstance();
+
+        fileBasedIndex.processAllKeys(PolicyIndex.INDEX_ID, key -> {
+                fileBasedIndex.processValues(
+                    PolicyIndex.INDEX_ID,
+                    key,
+                    null,
+                    (file, values) -> {
+                        policies.addAll(values);
+                        return true;
+                    },
+                    GlobalSearchScope.projectScope(project),
+                    IdFilter.getProjectIdFilter(project, false)
+                );
+                return true;
+            },
+            GlobalSearchScope.projectScope(project),
+            IdFilter.getProjectIdFilter(project, false)
+        );
+
+        return policies;
     }
 }
---
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/indexes/PolicyIndex.java
@@ -0,0 +1,120 @@
+package at.alirezamoh.whisperer_for_laravel.indexes;
+
+import at.alirezamoh.whisperer_for_laravel.indexes.dataExternalizeres.ListStringDataExternalizer;
+import at.alirezamoh.whisperer_for_laravel.support.utils.PhpClassUtils;
+import at.alirezamoh.whisperer_for_laravel.support.utils.PluginUtils;
+import at.alirezamoh.whisperer_for_laravel.support.utils.StrUtils;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.indexing.*;
+import com.intellij.util.io.DataExternalizer;
+import com.intellij.util.io.EnumeratorStringDescriptor;
+import com.intellij.util.io.KeyDescriptor;
+import com.jetbrains.php.lang.PhpFileType;
+import com.jetbrains.php.lang.psi.PhpFile;
+import com.jetbrains.php.lang.psi.elements.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+
+public class PolicyIndex extends FileBasedIndexExtension<String, List<String>> {
+    public static final ID<String, List<String>> INDEX_ID = ID.create("whisperer_for_laravel.policies");
+
+    private final List<String> POLICY_FOLDER_PATH = new ArrayList<>(List.of(
+        "/app/Policies/",
+        "app/Models/Policies/"
+    ));
+
+    @Override
+    public @NotNull ID<String, List<String>> getName() {
+        return INDEX_ID;
+    }
+
+    @Override
+    public @NotNull DataIndexer<String, List<String>, FileContent> getIndexer() {
+        return inputData -> {
+            Project project = inputData.getProject();
+
+            if (PluginUtils.shouldNotCompleteOrNavigate(project)) {
+                return Collections.emptyMap();
+            }
+
+            PsiFile file = inputData.getPsiFile();
+
+            if (!(file instanceof PhpFile phpFile)) {
+                return Collections.emptyMap();
+            }
+
+            Map<String, List<String>> policies = new HashMap<>();
+
+            for (PhpClass phpClass : PhpClassUtils.getPhpClassesFromFile(phpFile)) {
+                if (!phpClass.isAbstract()) {
+                    policies.put(phpClass.getFQN(), collectPolicies(phpClass));
+                }
+            }
+
+            return policies;
+        };
+    }
+
+    @Override
+    public @NotNull KeyDescriptor<String> getKeyDescriptor() {
+        return EnumeratorStringDescriptor.INSTANCE;
+    }
+
+    @Override
+    public @NotNull DataExternalizer<List<String>> getValueExternalizer() {
+        return new ListStringDataExternalizer();
+    }
+
+    @Override
+    public int getVersion() {
+        return 2;
+    }
+
+    @Override
+    public FileBasedIndex.@NotNull InputFilter getInputFilter() {
+        return file -> file.getFileType() == PhpFileType.INSTANCE && isPolicyClass(file);
+    }
+
+    @Override
+    public boolean dependsOnFileContent() {
+        return true;
+    }
+
+    @Override
+    public boolean traceKeyHashToVirtualFileMapping() {
+        return true;
+    }
+
+    private boolean isPolicyClass(VirtualFile file) {
+        String filePath = file.getPath();
+
+        filePath = filePath.replace(file.getName(), "");
+
+        for (String path : POLICY_FOLDER_PATH) {
+            if (filePath.contains(path)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private List<String> collectPolicies(PhpClass phpClass) {
+        List<String> policies = new ArrayList<>();
+
+        for (Method method : PhpClassUtils.getClassPublicMethods(phpClass, true)) {
+            if (StrUtils.isCamelCase(method.getName())) {
+                policies.add(StrUtils.snake(method.getName(), "-"));
+            }
+            else {
+                policies.add(method.getName());
+            }
+        }
+
+        return policies;
+    }
+}
