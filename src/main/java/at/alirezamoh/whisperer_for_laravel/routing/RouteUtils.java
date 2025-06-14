be79a37b092e6643d09a0469bfca4b81842c247d
Alireza Mohammadi <mohammadialireza238@gmail.com>
2025-06-14 21:20:50 +0200

    Adds annotator to validate existence of route name.

--- src/main/java/at/alirezamoh/whisperer_for_laravel/routing/RouteUtils.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/routing/RouteUtils.java
@@ -1,13 +1,19 @@
 package at.alirezamoh.whisperer_for_laravel.routing;
 
+import at.alirezamoh.whisperer_for_laravel.indexes.RouteIndex;
+import at.alirezamoh.whisperer_for_laravel.support.utils.MethodUtils;
 import at.alirezamoh.whisperer_for_laravel.support.utils.PhpClassUtils;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.util.indexing.IdFilter;
+import com.jetbrains.php.lang.psi.elements.FunctionReference;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.impl.ClassReferenceImpl;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class RouteUtils {
     /**
@@ -35,6 +41,28 @@ public class RouteUtils {
     }};
 
     /**
+     * The names of the route helper functions
+     */
+    private final static Map<String, Integer> ROUTE_NAME_METHODS = new HashMap<>() {{
+        put("route", 0);
+        put("to_route", 0);
+        put("signedRoute", 0);
+    }};
+
+    /**
+     * Classes to provide route names autocompletion
+     */
+    private final static String[] ROUTE_CLASSES = {"\\Illuminate\\Support\\Facades\\Redirect", "\\Illuminate\\Support\\Facades\\URL"};
+
+    /**
+     * Redirect and URL class methods
+     */
+    private final static Map<String, Integer> REDIRECT_AND_URL_METHODS = new HashMap<>() {{
+        put("route", 0);
+        put("signedRoute", 0);
+    }};
+
+    /**
      * Checks if the given method reference is a Laravel route method
      *
      * @param methodReference the method reference to check
@@ -59,5 +87,79 @@ public class RouteUtils {
         }
 
         return ROUTE_NAMESPACES.toArray(new String[0]);
+    }
+
+    /**
+     * Checks if the given PSI element is inside a `route` or `to_route` helper function call
+     * @param psiElement The PSI element to check
+     * @return           True or false
+     */
+    public static boolean isInsideCorrectRouteNameMethod(@NotNull PsiElement psiElement) {
+        MethodReference method = MethodUtils.resolveMethodReference(psiElement, 10);
+        FunctionReference function = MethodUtils.resolveFunctionReference(psiElement, 10);
+        Project project = psiElement.getProject();
+
+        return (
+            method != null
+                && isRouteParam(method, psiElement)
+                && PhpClassUtils.isCorrectRelatedClass(method, project, ROUTE_CLASSES)
+        )
+            || (function != null && isRouteParam(function, psiElement));
+    }
+
+
+    /**
+     * Retrieves all route names that match the given route name from the index
+     *
+     * @param routeName The route name to search for
+     * @param project   The current project
+     * @return A set of matching route names
+     */
+    public static Set<String> getMatchingRouteNames(String routeName, Project project) {
+        FileBasedIndex fileBasedIndex = FileBasedIndex.getInstance();
+        Set<String> matchingRouteNames = new HashSet<>();
+
+        fileBasedIndex.processAllKeys(RouteIndex.INDEX_ID, key -> {
+            int firstPipeIndex = key.indexOf(" | ");
+            if (firstPipeIndex != -1) {
+                int secondPipeIndex = key.indexOf(" | ", firstPipeIndex + 3);
+                if (secondPipeIndex != -1) {
+                    String keyRouteName = key.substring(firstPipeIndex + 3, secondPipeIndex);
+                    if (keyRouteName.equals(routeName)) {
+                        matchingRouteNames.add(key);
+                    }
+                }
+            }
+            return true;
+        }, GlobalSearchScope.projectScope(project), IdFilter.getProjectIdFilter(project, false));
+
+        return matchingRouteNames;
+    }
+
+    /**
+     * Check if the given reference and position match the route name parameter criteria
+     * @param reference The method or function reference
+     * @param position The PSI element position
+     * @return True or false
+     */
+    private static boolean isRouteParam(PsiElement reference, PsiElement position) {
+        String referenceName = (reference instanceof MethodReference)
+            ? ((MethodReference) reference).getName()
+            : ((FunctionReference) reference).getName();
+
+        if (referenceName == null) {
+            return false;
+        }
+
+        Integer expectedParamIndex = REDIRECT_AND_URL_METHODS.get(referenceName);
+        if (expectedParamIndex == null) {
+            expectedParamIndex = ROUTE_NAME_METHODS.get(referenceName);
+        }
+
+        if (expectedParamIndex == null) {
+            return false;
+        }
+
+        return MethodUtils.findParamIndex(position, false) == expectedParamIndex;
     }
 }
--- src/main/java/at/alirezamoh/whisperer_for_laravel/routing/routeName/RouteReferenceContributor.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/routing/routeName/RouteReferenceContributor.java
@@ -1,7 +1,6 @@
 package at.alirezamoh.whisperer_for_laravel.routing.routeName;
 
-import at.alirezamoh.whisperer_for_laravel.support.utils.MethodUtils;
-import at.alirezamoh.whisperer_for_laravel.support.utils.PhpClassUtils;
+import at.alirezamoh.whisperer_for_laravel.routing.RouteUtils;
 import at.alirezamoh.whisperer_for_laravel.support.utils.PluginUtils;
 import at.alirezamoh.whisperer_for_laravel.support.utils.PsiElementUtils;
 import com.intellij.openapi.project.Project;
@@ -9,40 +8,13 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
 import com.intellij.util.ProcessingContext;
-import com.jetbrains.php.lang.psi.elements.FunctionReference;
-import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This class allows the IDE to recognize and resolve route names
  */
 public class RouteReferenceContributor extends PsiReferenceContributor {
-    /**
-     * The names of the route helper functions
-     */
-    private final Map<String, Integer> ROUTE_METHODS = new HashMap<>() {{
-        put("route", 0);
-        put("to_route", 0);
-        put("signedRoute", 0);
-    }};
-
-    /**
-     * Classes to provide route names autocompletion
-     */
-    private final String[] ROUTE_CLASSES = {"\\Illuminate\\Support\\Facades\\Redirect", "\\Illuminate\\Support\\Facades\\URL"};
-
-    /**
-     * Redirect and URL class methods
-     */
-    private final Map<String, Integer> REDIRECT_AND_URL_METHODS = new HashMap<>() {{
-        put("route", 0);
-        put("signedRoute", 0);
-    }};
-
     /**
      * Registers the reference provider for route names
      * @param psiReferenceRegistrar The PSI reference registrar
@@ -64,7 +36,7 @@ public class RouteReferenceContributor extends PsiReferenceContributor {
                         return PsiReference.EMPTY_ARRAY;
                     }
 
-                    if(isInsideCorrectMethod(psiElement))
+                    if(RouteUtils.isInsideCorrectRouteNameMethod(psiElement))
                     {
                         return new PsiReference[]{
                             new RouteReference(
@@ -80,50 +52,5 @@ public class RouteReferenceContributor extends PsiReferenceContributor {
                 }
             }
         );
-    }
-
-    /**
-     * Checks if the given PSI element is inside a `route` or `to_route` helper function call
-     * @param psiElement The PSI element to check
-     * @return           True or false
-     */
-    private boolean isInsideCorrectMethod(@NotNull PsiElement psiElement) {
-        MethodReference method = MethodUtils.resolveMethodReference(psiElement, 10);
-        FunctionReference function = MethodUtils.resolveFunctionReference(psiElement, 10);
-        Project project = psiElement.getProject();
-
-        return (
-            method != null
-                && isRouteParam(method, psiElement)
-                && PhpClassUtils.isCorrectRelatedClass(method, project, ROUTE_CLASSES)
-            ) 
-            || (function != null && isRouteParam(function, psiElement));
-    }
-
-    /**
-     * Check if the given reference and position match the route name parameter criteria
-     * @param reference The method or function reference
-     * @param position The PSI element position
-     * @return True or false
-     */
-    private boolean isRouteParam(PsiElement reference, PsiElement position) {
-        String referenceName = (reference instanceof MethodReference)
-            ? ((MethodReference) reference).getName()
-            : ((FunctionReference) reference).getName();
-
-        if (referenceName == null) {
-            return false;
-        }
-
-        Integer expectedParamIndex = REDIRECT_AND_URL_METHODS.get(referenceName);
-        if (expectedParamIndex == null) {
-            expectedParamIndex = ROUTE_METHODS.get(referenceName);
-        }
-
-        if (expectedParamIndex == null) {
-            return false;
-        }
-
-        return MethodUtils.findParamIndex(position, false) == expectedParamIndex;
     }
 }
