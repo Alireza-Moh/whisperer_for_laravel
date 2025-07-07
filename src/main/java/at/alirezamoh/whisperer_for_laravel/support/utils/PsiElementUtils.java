3859b5a8228f1bc7f8ab849375cf2756bbe00bf4
Alireza Mohammadi <mohammadialireza238@gmail.com>
2025-07-07 20:45:08 +0200

    Removes unused import to clean up code.

--- src/main/java/at/alirezamoh/whisperer_for_laravel/support/utils/PsiElementUtils.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/support/utils/PsiElementUtils.java
@@ -10,7 +10,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
-import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.elements.impl.ArrayHashElementImpl;
 import org.jetbrains.annotations.NotNull;
