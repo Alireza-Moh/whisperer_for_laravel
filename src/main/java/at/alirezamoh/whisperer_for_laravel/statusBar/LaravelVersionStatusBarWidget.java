7fec30ab330c12ea94b1b043be1aeb4563bbc9cc
Alireza Mohammadi <mohammadialireza238@gmail.com>
2025-07-28 17:22:11 +0200

    Clean up.

--- src/main/java/at/alirezamoh/whisperer_for_laravel/statusBar/LaravelVersionStatusBarWidget.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/statusBar/LaravelVersionStatusBarWidget.java
@@ -5,7 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class LaravelVersionStatusBarWidget implements StatusBarWidget  {
-    private String versionText = "";
+    private String versionText;
 
     public LaravelVersionStatusBarWidget(String versionText) {
         this.versionText = versionText != null ? "Laravel: " + versionText : "";
