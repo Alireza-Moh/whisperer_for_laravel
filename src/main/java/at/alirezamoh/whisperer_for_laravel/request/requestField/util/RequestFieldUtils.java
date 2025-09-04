3e5c8652bfad966c09b0cba6c22678cd8c3f55ec
Alireza Mohammadi <mohammadialireza238@gmail.com>
2025-09-04 17:41:38 +0200

    Clean up.

--- src/main/java/at/alirezamoh/whisperer_for_laravel/request/requestField/util/RequestFieldUtils.java
+++ src/main/java/at/alirezamoh/whisperer_for_laravel/request/requestField/util/RequestFieldUtils.java
@@ -5,6 +5,7 @@ import at.alirezamoh.whisperer_for_laravel.support.utils.PhpClassUtils;
 import at.alirezamoh.whisperer_for_laravel.support.utils.PsiElementUtils;
 import at.alirezamoh.whisperer_for_laravel.support.utils.StrUtils;
 import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
@@ -218,8 +219,14 @@ final public class RequestFieldUtils {
         rules.forEach(rule -> {
             PsiElement key = rule.getKey();
             if (key instanceof StringLiteralExpression stringLiteral) {
+                LookupElementBuilder lookupElementBuilder = PsiElementUtils.buildSimpleLookupElement(
+                    StrUtils.removeQuotes(stringLiteral.getText())
+                );
                 resultSet.addElement(
-                    PsiElementUtils.buildSimpleLookupElement(StrUtils.removeQuotes(stringLiteral.getText()))
+                    PsiElementUtils.buildPrioritizedLookupElement(
+                        lookupElementBuilder,
+                        1000.0
+                    )
                 );
             }
         });
